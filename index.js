9d8a4ae090e5df974ef85792a19476c86dab40be
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-05-10 14:25:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function Point(x,y)
+{
+  this.x = x;
+  this.y = y;
+}
+
+Point.prototype.toString = function()
+{
+  return this.x + "," + this.y;
+}
+/////////////////////////////////////////////////////
+
+function Side(length)
+{
+    this.length = length;
+}
+////////////////////////////////////////////////////////
+
+function Shape()
+{
+}
+
+Shape.prototype.addToPlane = function(x,y)
+{
+  this.position = new Point(x,y);
+}
+
+Shape.prototype.move = function(x,y)
+{
+  this.position = new Point(x,y);
+}
